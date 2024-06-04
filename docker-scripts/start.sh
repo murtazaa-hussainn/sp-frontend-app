6c94f2e0a5e840a28d28f6cffe4ea111713c0227
murtazaa-hussainn <61433346+murtazaa-hussainn@users.noreply.github.com>
2024-06-04 06:21:04 +0500

    Create start.sh

---
+++ docker-scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 058264531795.dkr.ecr.us-east-1.amazonaws.com
+
+docker pull 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-frontend-app:latest
+
+docker run --env-file /opt/sp-frontend-app/.env -dit -p 4000:4000 --restart on-failure:5 --name frontend 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-frontend-app:latest
