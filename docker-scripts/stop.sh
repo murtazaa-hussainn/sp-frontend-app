39d387deb5ce7230238601293c0aea1ba7d0b361
murtazaa-hussainn <61433346+murtazaa-hussainn@users.noreply.github.com>
2024-06-05 05:24:56 +0500

    Update stop.sh

--- docker-scripts/stop.sh
+++ docker-scripts/stop.sh
@@ -3,7 +3,7 @@
 if [ $( docker ps -a | grep frontend | wc -l ) -gt 0 ]; then
   docker stop frontend
   docker rm frontend
-  docker rmi 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-frontend-app:latest
+  docker rmi 058264531795.dkr.ecr.us-east-1.amazonaws.com/sp-frontend-app-ecr:latest
 fi
 
 exit 0
